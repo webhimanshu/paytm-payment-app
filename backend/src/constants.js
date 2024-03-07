a90ae718725a6d6597e7a522799ea8376fb10652
webhimanshu <himanshusharma2709@gmail.com>
2024-03-07 12:44:25 +0530

    one way of db connect is done

---
+++ backend/src/constants.js
@@ -0,0 +1,3 @@
+const DB_NAME = 'paytm-payment-app'
+
+module.exports = { DB_NAME }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const DB_NAME = require('./constants');
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on('error', (error) => {
+            console.error("ERROR:", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, function () {
+            console.log('App is running on port', process.env.PORT)
+        })
+    } catch (error) {
+        console.error("ERROR:", error);
+        throw error;
+    }
+})();

94501a9d128eb9d890201433060f082d85daf152
webhimanshu <himanshusharma2709@gmail.com>
2024-03-08 11:35:25 +0530

    created routing file structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
+const mainRouter = require('./routes/index');
+const userRouter = require('./routes/user');
 app.use(cors({
     origin: process.env.CORS_ORIGIN
 }));
@@ -11,5 +12,8 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));// when we pass data in url it convert special char(space etc) into some charcter 
 app.use(express.static('public'));// sometime we want to store pdf and images on our server
 app.use(cookieParser());
+
+app.use('/api/v1' , mainRouter);
+app.use('/api/v1/user' , userRouter);
 
 module.exports = app;
